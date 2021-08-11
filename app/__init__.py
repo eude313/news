5275aa47d39c7d9d37e23a3a93b19effe08899c7
eude313 <eudes.charles@student.moringaschool.com>
2021-08-12 00:10:05 +0300

    add initial changes

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def base():
     return render_template('base.html')
 
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
